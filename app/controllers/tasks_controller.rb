421f902ee02cc055a8e02bea15217a4813d58de3
rakesh kumar <rakeshrajput537@gmail.com>
2019-02-13 18:17:35 +0530

    task completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
 	before_action :set_project
+	before_action :set_task, except: [:create]
 	def create
 		@task=@project.tasks.create(task_params)
 		redirect_to @project
@@ -15,9 +16,16 @@ class TasksController < ApplicationController
 		end
 		redirect_to @project
 	end
+	def complete
+		@task.update_attribute(:completed_at, Time.now)
+		redirect_to @project , notice: "Task completed"
+	end
 	private
 	def set_project
 		@project=Project.find(params[:project_id])		
+	end
+	def set_task
+		@task=@project.tasks.find(params[:id])
 	end
 	def task_params
 		params[:task].permit(:content)
